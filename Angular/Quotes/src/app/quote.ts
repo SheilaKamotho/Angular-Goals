c61c76197c911599e6fd8c578494c4f55921a4d9
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-21 13:56:10 +0300

    add a goal in the goal component

--- Angular/Quotes/src/app/quote.ts
+++ Angular/Quotes/src/app/quote.ts
@@ -3,4 +3,4 @@ export class Quote {
     quote: string;
     author: string;
     submittedBy: string;
-}
+  }
--- Angular/Quotes/src/app/quote/quote.component.ts
+++ Angular/Quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {id:1, quote:'Worrying means suffering twice', author:'J.K Rowling',submittedBy:'Sheila Kamotho'},
+  
+  ];
 
   constructor() { }
 
